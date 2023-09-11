1f2e62f39b3096eb4f22557fa690fdba663b5dda
Omar Bracamontes <omar@aresmaterials.com>
2023-09-11 17:38:53 -0600

    minor: pasé las funciones de prueba a un archivo separado para probar con diferentes métodos

--- 1_Optimization/Line_search/bfgs.py
+++ 1_Optimization/Line_search/bfgs.py
@@ -1,6 +1,7 @@
 import numpy as np
 import finite_differences as fi
 import matplotlib.pyplot as plt
+import test_functions as tf
 '''
     BFGS
 '''
@@ -129,88 +130,13 @@ def plot_optimization(X0,f_name,K,title='',tau=1.,rho=0.9,take_strong=False):
 
 # Testing
 if '__main__'==__name__:
-    #
-    # Test Functions
-    #
-    def sphere(X,c=1.0):
-        '''
-            Translated Sphere's function
-                f = f(x): \R^n --> \R
-            
-            Input:
-                X (numpy array): vector with dimension nx1
-                c (float): The center of the sphere. It is assumed that the center would be an uniform vector.
-            Output:
-                (float): The value of the function evaluated on X
-            
-        '''
-        C = np.full_like(X,c, dtype=float)
-        return np.sum(np.square(np.subtract(X,C)))
-    def rosenbrock(X):
-        '''
-            Rosenbrock's function
-                f = f(x): \R^n --> \R
-            
-            Input:
-                X (numpy array): vector with dimension nx1
-            Output:
-                (float): The value of the function evaluated on X
-            
-        '''
-        return np.sum([100 * (X[i+1] - X[i]**2)**2 + (X[i] - 1)**2 for i in range(X.shape[0]-1)])
-    def perm(X,b=1):
-        '''
-            Perm's function
-                f = f(x): \R^n --> \R
-            
-            Input:
-                X (numpy array): vector with dimension nx1
-                b (float): just a perm's constant
-            Output:
-                (float): The value of the function evaluated on X
-            
-        '''
-        dim = X.shape[0]
-        def _j_sum_addend(x_j,i,j):
-            return (j**i + b) * ( ( x_j/j )**i - 1) 
-        def _j_sum(X,i):
-            return sum([ _j_sum_addend(X[j-1],i,j)for j in range(1,dim+1)])
-        
-        return sum([ _j_sum(X,i)**2 for i in range(1,dim+1)])
-
-    # Initialize Tests
-    dimension = 5
-    X0 = np.full((dimension,),0.5, dtype=float)
-    argmin_params = {
-        'sphere':{
-            'f':sphere,# Function
-            'x':X0, # Numpy array
-            'epochs':10,
-            'step_size':0.5,
-            'title':'Translated Sphere in $R^n$',
-        },
-        'rosen':{
-            'f':rosenbrock,# Function
-            'x':X0, # Numpy array
-            'epochs':50, #100000 pero es muy lento
-            'step_size':1e-3,
-            'title':'Rosenbrock in $R^n$',
-        },
-        'perm':{
-            'f':perm,# Function
-            'x':X0, # Numpy array
-            'epochs':1000, #300000 pero es muy lento
-            'step_size':1e-8,
-            'title':'Perm in $R^n$',
-        },
-    }
     
     plot_optimization(
-        argmin_params['perm']['x'], 'perm',K=argmin_params['perm']['epochs'],
-        title=argmin_params['perm']['title'],
+        tf.argmin_params['perm']['x'], 'perm',K=tf.argmin_params['perm']['epochs'],
+        title=tf.argmin_params['perm']['title'],
         tau=1.,rho=0.9,take_strong=False
     )
-    # for function_name,function_params in argmin_params.items():
+    # for function_name,function_params in tf.argmin_params.items():
     #     plot_optimization(
     #         function_params['x'], function_name,K=function_params['epochs'],
     #         title=function_params['title'],
---
+++ 1_Optimization/Line_search/test_functions.py
@@ -0,0 +1,76 @@
+import numpy as np
+#
+# Test Functions
+#
+def sphere(X,c=1.0):
+    '''
+        Translated Sphere's function
+            f = f(x): \R^n --> \R
+        
+        Input:
+            X (numpy array): vector with dimension nx1
+            c (float): The center of the sphere. It is assumed that the center would be an uniform vector.
+        Output:
+            (float): The value of the function evaluated on X
+        
+    '''
+    C = np.full_like(X,c, dtype=float)
+    return np.sum(np.square(np.subtract(X,C)))
+def rosenbrock(X):
+    '''
+        Rosenbrock's function
+            f = f(x): \R^n --> \R
+        
+        Input:
+            X (numpy array): vector with dimension nx1
+        Output:
+            (float): The value of the function evaluated on X
+        
+    '''
+    return np.sum([100 * (X[i+1] - X[i]**2)**2 + (X[i] - 1)**2 for i in range(X.shape[0]-1)])
+def perm(X,b=1):
+    '''
+        Perm's function
+            f = f(x): \R^n --> \R
+        
+        Input:
+            X (numpy array): vector with dimension nx1
+            b (float): just a perm's constant
+        Output:
+            (float): The value of the function evaluated on X
+        
+    '''
+    dim = X.shape[0]
+    def _j_sum_addend(x_j,i,j):
+        return (j**i + b) * ( ( x_j/j )**i - 1) 
+    def _j_sum(X,i):
+        return sum([ _j_sum_addend(X[j-1],i,j)for j in range(1,dim+1)])
+    
+    return sum([ _j_sum(X,i)**2 for i in range(1,dim+1)])
+
+# Initialize test parameters
+dimension = 5
+X0 = np.full((dimension,),0.5, dtype=float)
+argmin_params = {
+    'sphere':{
+        'f':sphere,# Function
+        'x':X0, # Numpy array
+        'epochs':10,
+        'step_size':0.5,
+        'title':'Translated Sphere in $R^n$',
+    },
+    'rosen':{
+        'f':rosenbrock,# Function
+        'x':X0, # Numpy array
+        'epochs':50, #100000 pero es muy lento
+        'step_size':1e-3,
+        'title':'Rosenbrock in $R^n$',
+    },
+    'perm':{
+        'f':perm,# Function
+        'x':X0, # Numpy array
+        'epochs':1000, #300000 pero es muy lento
+        'step_size':1e-8,
+        'title':'Perm in $R^n$',
+    },
+}
